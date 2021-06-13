7c0431ee437abd9426822f8779aafb2d7a9c0492
ctrl-gaurav <gauravhhh30@gmail.com>
2021-06-13 10:12:26 +0530

    Firebase Initialized

--- BeachMates - App/lib/main.dart
+++ BeachMates - App/lib/main.dart
@@ -3,12 +3,19 @@ import 'package:b2b/widgets/tab_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]).then((val) {
+    debugPrint('main');
+    FirebaseAuth.instance.authStateChanges().listen((event) {
+      debugPrint('event ${event.toString()}');
+    });
     runApp(MyApp());
   });
 }
