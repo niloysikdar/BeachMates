92b4fc5be8eecde0285c34326629f93af1f84045
ctrl-gaurav <gauravhhh30@gmail.com>
2021-06-13 07:06:12 +0530

    Second Screen Added

---
+++ BeachMates - App/beach_mates/lib/screens/second_screen/SecondScreen.dart
@@ -0,0 +1,63 @@
+import 'package:b2b/widgets/live_events_cards.dart';
+import 'package:flutter/material.dart';
+import 'package:b2b/widgets/live_events_header.dart';
+
+class SecondScreen extends StatefulWidget {
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              liveEventsHeader(size: size),
+              sortText(),
+              Column(
+                children: [
+                  LiveEventsCard(
+                    size: size,
+                    imagepath: "assets/images/volleyball.png",
+                    title: "Looking for more players",
+                    description: "I have a ball and volleyball net",
+                  ),
+                  LiveEventsCard(
+                    size: size,
+                    imagepath: "assets/images/jetski.png",
+                    title: "Race Some Jet Skiis",
+                    description: "Wanna have a  200m Race.",
+                  ),
+                  LiveEventsCard(
+                    size: size,
+                    imagepath: "assets/images/sandcastle.png",
+                    title: "Build some Sand Castles",
+                    description: "Lets build a huge City Fort",
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget sortText() {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Text(
+        "Sort by: Newest",
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
